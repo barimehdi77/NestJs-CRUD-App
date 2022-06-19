f805bdf70f59eccb3a3dee54d571163eb245dd62
barimehdi77 <mbari@student.1337.ma>
2022-06-19 09:40:59 -0400

    move .env inside api folder

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
